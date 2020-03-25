11645794f24f8a3a554afbf1b9cfa513c93add5c
Michael Garza <migarza@fb.com>
2020-03-25 17:03:08 -0500

    initial commit

---
+++ day_2_challenge.js
@@ -0,0 +1,36 @@
+// day 2 challenge 1
+
+console.log("------ Challenge # 1 -----")
+
+let numberPrinter = input => {
+    for (let i = 1; i <= input; i++){
+        console.log("Iteration #", i);
+    }
+}
+numberPrinter(10);
+
+console.log("------ Challenge # 2 ------")
+
+// day 2 challenge 2
+
+let three = 3;
+let five = 5;
+let fifteen =15;
+
+let fizzBuzz = input => {
+    for (let i = 1; i <= input; i++){
+        if (i % fifteen == 0){
+            console.log(i, `FizzBuzz`)
+        }
+        else if( i % three == 0){
+            console.log(i, `Fizz`)
+        }
+        else if (i % five == 0){
+            console.log(i, `Buzz`);
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+fizzBuzz(100);
